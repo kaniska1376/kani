123ba980aeed3a0d8e9d8ef67ac73eeb359ceaa6
kaniska1376 <727723euec080@skcet.ac.in>
2025-01-29 15:43:31 +0530

    mern

---
+++ obj.js
@@ -0,0 +1,8 @@
+/*let mark=60;
+let grade=(mark>=90)?"A":(mark>70)?"B":(mark>=40)?"C"
+:"F";
+console.log(grade);*/
+let array=["hello"]
+let arr2=[90,91,92,93,94]
+let arrCopy=[...array,...arr2]
+console.log(arrCopy);
